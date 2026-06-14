7b3ec40fe9b5898a47d5f29fe89e006eacbaa834
Nayely Ayol <nayely.ayol@epn.edu.ec>
2026-06-14 00:20:12 -0500

    Trabajo completo

---
+++ formulario/app2.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect
+import mysql.connector
+
+app = Flask(__name__)
+
+conexion = mysql.connector.connect(
+    host="mysql_principal",
+    user="root",
+    password="root",
+    database="replica"
+)
+
+@app.route("/")
+def index():
+    cursor = conexion.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM peliculas")
+    peliculas = cursor.fetchall()
+    return render_template("index.html", peliculas=peliculas)
+
+@app.route("/agregar", methods=["POST"])
+def agregar():
+    titulo = request.form["titulo"]
+    director = request.form["director"]
+    genero = request.form["genero"]
+    anio = request.form["anio"]
+
+    cursor = conexion.cursor()
+    cursor.execute(
+        "INSERT INTO peliculas (titulo, director, genero, anio) VALUES (%s, %s, %s, %s)",
+        (titulo, director, genero, anio)
+    )
+    conexion.commit() # Guarda permanentemente los cambios realizados en la base de datos.
+
+    return redirect("/") #envía al navegador nuevamente a la ruta principal
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8100, debug=True)
